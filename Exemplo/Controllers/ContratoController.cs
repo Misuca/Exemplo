7222add33aeff3ec99f0e45019a95aaf12a164d0
miguel gomes <miguelg2001@hotmail.com>
2020-04-15 22:42:18 +0100

    2 commit

---
+++ Exemplo/Controllers/ContratoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Exemplo.Models;
+
+namespace Exemplo.Controllers
+{
+    public class ContratoController : Controller
+    {
+        private Gestão_de_Frota_de_AutomoveisEntities db = new Gestão_de_Frota_de_AutomoveisEntities();
+
+        // GET: Contrato
+        public ActionResult Index()
+        {
+            return View(db.Contrato.ToList());
+        }
+
+        // GET: Contrato/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contrato contrato = db.Contrato.Find(id);
+            if (contrato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contrato);
+        }
+
+        // GET: Contrato/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Contrato/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Contrato,NomeFornecedor,PedidoCompra,NºProcedimento")] Contrato contrato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Contrato.Add(contrato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contrato);
+        }
+
+        // GET: Contrato/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contrato contrato = db.Contrato.Find(id);
+            if (contrato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contrato);
+        }
+
+        // POST: Contrato/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Contrato,NomeFornecedor,PedidoCompra,NºProcedimento")] Contrato contrato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contrato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contrato);
+        }
+
+        // GET: Contrato/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Contrato contrato = db.Contrato.Find(id);
+            if (contrato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contrato);
+        }
+
+        // POST: Contrato/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contrato contrato = db.Contrato.Find(id);
+            db.Contrato.Remove(contrato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Exemplo/Controllers/UtilizadorController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Exemplo.Models;
+
+namespace Exemplo.Controllers
+{
+    public class UtilizadorController : Controller
+    {
+        private Gestão_de_Frota_de_AutomoveisEntities db = new Gestão_de_Frota_de_AutomoveisEntities();
+
+        // GET: Utilizador
+        public ActionResult Index()
+        {
+            return View(db.Utilizador.ToList());
+        }
+
+        // GET: Utilizador/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador utilizador = db.Utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador);
+        }
+
+        // GET: Utilizador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilizador/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Utilizador,Nome,Departamento,Cargo,Telefone,CC,Morada,CodigoPostal,Localidade")] Utilizador utilizador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilizador.Add(utilizador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(utilizador);
+        }
+
+        // GET: Utilizador/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador utilizador = db.Utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador);
+        }
+
+        // POST: Utilizador/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Utilizador,Nome,Departamento,Cargo,Telefone,CC,Morada,CodigoPostal,Localidade")] Utilizador utilizador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilizador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(utilizador);
+        }
+
+        // GET: Utilizador/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilizador utilizador = db.Utilizador.Find(id);
+            if (utilizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilizador);
+        }
+
+        // POST: Utilizador/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Utilizador utilizador = db.Utilizador.Find(id);
+            db.Utilizador.Remove(utilizador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
